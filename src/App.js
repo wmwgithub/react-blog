8e4f9f7534703ee47533be3d886be5925db1cb78
王旻伟 <3209304627@qq.com>
2019-03-21 17:26:36 +0800

    init:构建项目目录

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.less';
+import  {Button} from 'antd';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <Button>src/App.js</Button> and save to reload.
+          </p>
+        </header>
+        <Button type='primary' shape='circle' icon='search'></Button>
+        <Button type='primary' icon='search'>Search</Button>
+      </div>
+    );
+  }
+}
+export default App
