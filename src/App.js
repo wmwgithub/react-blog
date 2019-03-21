4b860be49bdd4ec47946a4ed87ac7589f9bd39d1
王旻伟 <3209304627@qq.com>
2019-03-21 19:05:24 +0800

    init:构建项目目录

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
             Edit <Button>src/App.js</Button> and save to reload.
           </p>
         </header>
+        <hr/>
         <Button type='primary' shape='circle' icon='search'></Button>
+        <hr/>
         <Button type='primary' icon='search'>Search</Button>
       </div>
     );
